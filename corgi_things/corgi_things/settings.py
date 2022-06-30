647273e8c1ed3af5b8e29785666f35b4b4d683b9
Andra Popa <andrapopa272@gmail.com>
2022-06-30 23:27:17 +0300

    added pipeline for mongodb

--- corgi_things/corgi_things/settings.py
+++ corgi_things/corgi_things/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'corgi_things.pipelines.CorgiThingsPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'corgi_things.pipelines.CorgiThingsPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
