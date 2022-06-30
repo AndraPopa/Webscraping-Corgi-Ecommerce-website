0255a9cff34b21bf6c74d2b09005ffd861eff669
Andra Popa <andrapopa272@gmail.com>
2022-06-30 14:24:59 +0300

    retrieved links to products

--- corgi_things/corgi_things/spiders/corgi.py
+++ corgi_things/corgi_things/spiders/corgi.py
@@ -1,10 +1,14 @@
 import scrapy
-
+from corgi_things.corgi_things.items import CorgiThingsItem
 
 class CorgiSpider(scrapy.Spider):
     name = 'corgi'
     allowed_domains = ['corgithings.com']
-    start_urls = ['http://corgithings.com/']
+    start_urls = ['https://corgithings.com/collections/shop']
 
     def parse(self, response):
+        corgi_product_link = response.css('div.product-title::attr(href)')
+        yield from response.follow_all(corgi_product_link, callback=self.parse_corgi_item)
+
+    def parse_corgi_item(self):
         pass
