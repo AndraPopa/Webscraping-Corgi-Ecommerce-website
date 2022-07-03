eb7c59c582ce9357ac9be6e75606735a06663bd7
Andra Popa <andrapopa272@gmail.com>
2022-07-03 15:52:13 +0300

    added user-agents rotation

--- corgi_things/corgi_things/settings.py
+++ corgi_things/corgi_things/settings.py
@@ -1,3 +1,5 @@
+from scrapy.settings.default_settings import DOWNLOADER_MIDDLEWARES
+
 BOT_NAME = 'corgi_things'
 
 SPIDER_MODULES = ['corgi_things.spiders']
@@ -11,4 +13,23 @@ ITEM_PIPELINES = {
 }
 IMAGES_STORE = 's3://store-the-corgi-pictures-23021998/images/'
 IMAGES_STORE_S3_ACL = 'public-read'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
+}
+
+USER_AGENTS = [
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/57.0.2987.110 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Linux x86_64) '
+     'AppleWebKit/537.36 (KHTML, like Gecko) '
+     'Chrome/61.0.3163.79 '
+     'Safari/537.36'),  # chrome
+    ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+     'Gecko/20100101 '
+     'Firefox/55.0')  # firefox
+]
 
